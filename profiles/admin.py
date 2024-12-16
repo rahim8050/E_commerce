cd3deb19def9324db1dd87b17fc98e3eb1679610
rahim8050 <rahimranxx8050@gmail.com>
2024-12-16 13:24:29 +0000

    update

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = 'Profile'
+admin.site.site_title = 'profile'
+from .models import Profile
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['content','user']
+    search_fields = ['content','bio']
+    list_filter = ['user']
+    list_per_page = 25
+admin.site.register(Profile,ProfileAdmin)
--- profiles/models.py
+++ profiles/models.py
@@ -7,3 +7,7 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'profiles'
