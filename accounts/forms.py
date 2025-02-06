66f437ec690aac495697f975d314fd1bf46bd5ce
rahim8050 <rahimranxx8050@gmail.com>
2025-02-06 06:41:29 +0000

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,5 +7,10 @@ class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput)
     def clean(self):
+    def clean_username(self):
         username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username__iexact=username)
+        if not qs.exists():
+            raise forms.ValidationError('Username not found')
+        return username
         password = self.cleaned_data.get('password')
